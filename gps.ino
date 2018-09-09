e314ed379bb6e848f69a6feeaca98263c135458d
VioletGiraffe <alexxxx89@ya.ru>
2018-09-09 10:59:10 +0300

    Spaces -> Tabs

--- gps.ino
+++ gps.ino
@@ -11,9 +11,9 @@ TinyGPSPlus gps;
 
 #define sck 3
 #define mosi 2
-#define dc   4
-#define cs   5
-#define rst  6
+#define dc 4
+#define cs 5
+#define rst 6
 Adafruit_SSD1351 tft(cs, dc, mosi, sck, rst);
 //Adafruit_ST7735 tft(cs, dc, mosi, sck, rst);
 
@@ -22,111 +22,115 @@ Adafruit_SSD1351 tft(cs, dc, mosi, sck, rst);
 
 inline void blinkLED()
 {
-  static bool ledOn = false;
-  ledOn = !ledOn;
-  if (ledOn)
-    PIOB->PIO_SODR=1<<27; //lights the LED
-  else
-    PIOB->PIO_CODR=1<<27; //clears it
+	static bool ledOn = false;
+	ledOn = !ledOn;
+	if (ledOn)
+		PIOB->PIO_SODR = 1 << 27; //lights the LED
+	else
+		PIOB->PIO_CODR = 1 << 27; //clears it
 }
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial2.begin(GPSBaud);
- // tft.initR(INITR_144GREENTAB);
-  tft.begin();
-  tft.fillScreen(0x0000);
-  tft.setTextSize(1);
-  tft.setTextColor(0xFFFF, 0x0000);
-  tft.setTextWrap(true);
+	Serial.begin(115200);
+	Serial2.begin(GPSBaud);
+	// tft.initR(INITR_144GREENTAB);
+	tft.begin();
+	tft.fillScreen(0x0000);
+	tft.setTextSize(1);
+	tft.setTextColor(0xFFFF, 0x0000);
+	tft.setTextWrap(true);
 
-  pinMode(13, OUTPUT);
+	pinMode(13, OUTPUT);
 
-  Timer3.attachInterrupt(&blinkLED);
+	Timer3.attachInterrupt(&blinkLED);
 }
 
 void loop()
 {
-  // This sketch displays information every time a new sentence is correctly encoded.
-  while (Serial2.available() > 0)
-  {
-    if (gps.encode(Serial2.read()))
-      displayInfo();
-  }
+	// This sketch displays information every time a new sentence is correctly encoded.
+	while (Serial2.available() > 0)
+	{
+		if (gps.encode(Serial2.read()))
+			displayInfo();
+	}
 }
 
 void displayInfo()
 {
-  static uint32_t nSatsPrevious = ~0;
+	static uint32_t nSatsPrevious = ~0;
 
-  tft.setCursor(0, 0);
+	tft.setCursor(0, 0);
 
-  const auto nSats = gps.satellites.value();
-  if (nSats != nSatsPrevious)
-  {
-    if (nSats == 0)
-    {
-      tft.setTextColor(0xFFE0, 0x0000);
-      Timer3.stop();
-      Timer3.setFrequency(20.0);
-      Timer3.start();
-    }
-    else if (nSats >= 4)
-    {
-      tft.setTextColor(0x07E0, 0x0000);
-      Timer3.stop();
-      Timer3.setFrequency(7.0);
-      Timer3.start();
-    }
-    else
-    {
-      tft.setTextColor(0xFFFF, 0x0000);
-      Timer3.stop();
-      Timer3.setFrequency(2.0);
-      Timer3.start();
-    }
-  }
+	const auto nSats = gps.satellites.value();
+	if (nSats != nSatsPrevious)
+	{
+		if (nSats == 0)
+		{
+			tft.setTextColor(0xFFE0, 0x0000);
+			Timer3.stop();
+			Timer3.setFrequency(20.0);
+			Timer3.start();
+		}
+		else if (nSats >= 4)
+		{
+			tft.setTextColor(0x07E0, 0x0000);
+			Timer3.stop();
+			Timer3.setFrequency(7.0);
+			Timer3.start();
+		}
+		else
+		{
+			tft.setTextColor(0xFFFF, 0x0000);
+			Timer3.stop();
+			Timer3.setFrequency(2.0);
+			Timer3.start();
+		}
+	}
 
-  nSatsPrevious = nSats;
+	nSatsPrevious = nSats;
 
-  PRINT(F("# of sats: "));
-  PRINTLN(nSats, 2);
+	PRINT(F("# of sats: "));
+	PRINTLN(nSats, 2);
 
-  PRINT(F(" Location: ")); 
-  if (gps.location.isValid())
-  {
-    PRINT(gps.location.lat(), 6);
-    PRINT(F(","));
-    PRINTLN(gps.location.lng(), 6);
-  }
-  else
-  {
-     PRINTLN(F("INVALID"));
-  }
+	PRINT(F(" Location: "));
+	if (gps.location.isValid())
+	{
+		PRINT(gps.location.lat(), 6);
+		PRINT(F(","));
+		PRINTLN(gps.location.lng(), 6);
+	}
+	else
+	{
+		PRINTLN(F("INVALID"));
+	}
 
-  if (gps.time.isValid())
-  {
-    PRINT(F(" Time: "));
-    if (gps.time.hour() < 10) PRINT(F("0"));
-    PRINT(gps.time.hour());
-    PRINT(F(":"));
-    if (gps.time.minute() < 10) PRINT(F("0"));
-    PRINT(gps.time.minute());
-    PRINT(F(":"));
-    if (gps.time.second() < 10) PRINT(F("0"));
-    PRINT(gps.time.second());
-    PRINT(F("."));
-    if (gps.time.centisecond() < 10) PRINT(F("0"));
-    PRINTLN(gps.time.centisecond());
-  }
-  else
-    PRINTLN(F("INVALID"));
+	if (gps.time.isValid())
+	{
+		PRINT(F(" Time: "));
+		if (gps.time.hour() < 10)
+			PRINT(F("0"));
+		PRINT(gps.time.hour());
+		PRINT(F(":"));
+		if (gps.time.minute() < 10)
+			PRINT(F("0"));
+		PRINT(gps.time.minute());
+		PRINT(F(":"));
+		if (gps.time.second() < 10)
+			PRINT(F("0"));
+		PRINT(gps.time.second());
+		PRINT(F("."));
+		if (gps.time.centisecond() < 10)
+			PRINT(F("0"));
+		PRINTLN(gps.time.centisecond());
+	}
+	else
+		PRINTLN(F("INVALID"));
 
-  if (gps.speed.isValid())
-  {
-    PRINT(F(" "));
-    PRINT(gps.speed.kmph());
-    PRINTLN(F(" kph"));
-  }
+	if (gps.speed.isValid())
+	{
+		PRINT(F(" "));
+		PRINT(gps.speed.kmph());
+		PRINTLN(F(" kph"));
+	}
 }
